0b192734a778ff238c1eae926bd82c854a76e8ed
anumolvalsan <74457216+anumolvalsan@users.noreply.github.com>
2020-11-19 13:14:58 +0400

    Add files via upload

---
+++ day3_Iteration.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+
+
+export default class Day3_Iteration extends LightningElement {
+    players=[
+        {
+            Id: 1,
+            Name: 'Sachin Tendulkar',
+            Email: 'sachin@sachin.com',
+        },
+        {
+            Id: 2,
+            Name: 'Sourav Ganguly',
+            Email: 'Sourav@Sourav.com',
+        },
+        {
+            Id: 3,
+            Name: 'Rahul Dravid',
+            Email: 'rd@rd.com',
+        },
+        {
+            Id: 4,
+            Name: 'Anil Kuble',
+            Email: 'ak@ak.com',
+        }
+    ];
+
+}
